22c0c1ebafefa3a832ef8ade5e57df1430d4ec04
HarshPulkit08 <harshpulkit08@gmail.com>
2026-01-09 13:26:46 +0530

    assignment2 pt2

---
+++ assignment2/Task3.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a number you wanted to check for even or odd: "))
+if num%2==0:
+    print(f"{num} is an Even number")
+else:
+    print(f"{num} is an Odd number")
+    
---
+++ assignment2/Task4.py
@@ -0,0 +1,4 @@
+sum = 0
+for i in range(0, 50):
+    sum = sum+i
+print("The sum of number from 0 to 50 is:", sum)
